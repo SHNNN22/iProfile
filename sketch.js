7dc49144638dfca2889416d91ccc07378a568037
SHNNN22 <shnnncnnr@gmail.com>
2019-01-06 22:16:25 +0100

    updated 3m version

--- sketch.js
+++ sketch.js
@@ -1,45 +1,62 @@
 // GLOBALS
-const topDiv = document.querySelector('#topDiv')
+//const topDiv = document.querySelector('#topDiv')
 let canvas
 let unitsBar
 let totalLength
-let moduleLength
-let module
+const moduleLength = 280
+let singleModule
 const x = 0
-const y = 150
+const y = 230
 const h = 55
 let mod8Num
 let restModule
 let textBox
-let slider
-const segment = 4480
+let shrink
+let pan
+
+let connectors
+let suspensions
 let clips
+
+let module10Num
+let shortBruto
+let shortNet
+
+const max3m = 2800
+let unitsAmount
+let shortMod
+
 let a, b, result
+
 
 
 //SETUP
 function setup() {
-  canvas = createCanvas(windowWidth, 300)
-
+  canvas = createCanvas(windowWidth, 350)
+  output = createP(' ')
   // infos
   textBox = createInput('');
   textBox.attribute('placeholder', '15000')
+	textBox.attribute('oninput', 'javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength)')
   textBox.attribute('type', 'number')
-  textBox.attribute('min', '560')
-  textBox.attribute('max', '100000')
+	textBox.attribute('step', '280')
+	textBox.attribute('min', '0')
+	textBox.attribute('maxlength', '5')
+	textBox.attribute('id', 'focus')
+	document.getElementById("focus").autofocus = true
   textBox.parent(topDiv)
   let insert = createElement('strong', 'insert distance')
   insert.parent(topDiv)
 
   // shrink
   createElement('strong', 'shrink')
-  slider = createSlider(22, 100, 30)
+  shrink = createSlider(22, 100, 22)
 
   // pan
-  camera = createSlider(-1000, 0, 0)
+  pan = createSlider(-1500, 0, 0)
   createElement('strong', 'pan')
 
-  // calculator
+	// calculator
   plus = createButton('+').parent('#calculator')
   minus = createButton('-').parent('#calculator')
   times = createButton('*').parent('#calculator')
@@ -79,97 +96,334 @@ function div() {
   result = a / b
   para.html(result)
 }
-
 // WINDOW RESIZED
 function windowResized() {
-  resizeCanvas(windowWidth, 300)
+  resizeCanvas(windowWidth, 350)
 }
 
 // Find total number of single modules
 function modulesNum() {
-  let n = totalLength / moduleLength
-  return int(n)
+  let unitNum = totalLength / moduleLength
+  return int(unitNum)
 }
 
 // DRAW
 function draw() {
   background(51)
 
-  moduleLength = 280
-  sliderLength = moduleLength / slider.value()
+  sliderLength = moduleLength / shrink.value()
   totalLength = textBox.value()
 
   //texts
-  textSize(15)
+  textSize(12)
   fill(222)
   noStroke()
-  text('Total length: ' + totalLength + ' mm', x, y - 110)
+  text(`Total length: ${totalLength} mm`, x, 20)
   let profileLength = modulesNum() * 280
-  text('Profile length: ' + profileLength + ' mm', x, y - 90)
+  text(`Profile length: ${profileLength} mm`, x, 40)
+
   let leftover = totalLength - profileLength
-  text('Leftover: ' + leftover + ' mm', x, y - 70)
+  text(`Leftover: ${leftover} mm`, x, 60)
 
-  // CONNECTORS
-  if (totalLength < 6160) {
+  // Connectors 3m - less cuts
+  if (totalLength < 3080) {
     connectors = 0
-  } else if (profileLength >= 6160 && profileLength < 11760) {
+  } else if (profileLength >= 3080 && profileLength < 5880) {
     connectors = 1
-  } else if (profileLength >= 11760 && profileLength < 15120) {
-    connectors = 2
+  } else if (profileLength % max3m == 0) {
+    connectors = Math.floor(profileLength / max3m) - 1
   } else {
-    connectors = Math.floor(profileLength / segment)
+    connectors = Math.floor(profileLength / max3m)
   }
-  text('180Corner: ' + connectors, x, y - 50)
 
-  // CLIPS
-  clips = Math.floor(profileLength / 600) + 1
-  if (clips < 2) {
-    clips = 2
-  }
-  text('Clips: ' + clips, x, y - 30)
-
-  // SUSPENSION
-  suspensions = Math.floor(profileLength / 1000)
-  if (suspensions < 3) {
+  // Suspension 3m - less cuts
+  if (modulesNum() < 7) {
     suspensions = 2
+  } else if (profileLength % max3m == 0) {
+    suspensions = module10Num * 3
+  } else {
+    suspensions = module10Num * 3 + 2
   }
-  text('Suspensions: ' + suspensions, x, y - 10)
 
-  textSize(12)
+  text(`180Corner: ${connectors}    •    Suspensions: ${suspensions}`, x, y - h - 10)
+
+  // Connectors 3m - less price
+  if (profileLength < 3080) {
+    connectors2 = 0
+  } else if (profileLength % 2240 == 0 || profileLength % 2240 == 280) {
+    connectors2 = mod8Num - 1
+  } else {
+    connectors2 = mod8Num
+  }
+
+  // Clips
+  if (profileLength % 2240 == 0) {
+    clips = mod8Num * 3
+  } else {
+    clips = mod8Num * 3 + 2
+  }
+
+  if (profileLength > 2240 && profileLength < 3080) {
+    text(`180Corner: ${connectors2}    •    SRL TR clips: ${clips}    •    SRL 85 / MIC 60 clips: ${clips-2}`, x, y + h * 1.8)
+  } else if ((modulesNum() - (mod8Num * 8)) == 1) {
+    text(`180Corner: ${connectors2}    •    Clips: ${clips - 3}`, x, y + h * 1.8)
+  } else {
+    text(`180Corner: ${connectors2}    •    Clips: ${clips}`, x, y + h * 1.8)
+  }
+
+  // Long modules 3m - less cuts
+  module10Num = Math.floor(profileLength / max3m)
+  shortBruto = (profileLength - max3m * module10Num)
+  unitsAmount = Math.floor(shortBruto / moduleLength)
+
+  for (let i = 0; i < module10Num; i++) {
+    textSize(1 * sliderLength)
+
+		push()
+    stroke(0)
+    fill(71, 115, 137)
+    let modL = rect(i * (sliderLength * 10) + pan.value(), y - h, sliderLength * 8, h)
+		pop()
+
+    noStroke()
+    fill(0, 0, 0)
+    text(`${i+1}
+		224`, i * (sliderLength * 10) + pan.value() + 4, y - h + 20);
+
+		push()
+    stroke(0)
+    fill(237, 255, 171)
+    let modc = rect((i * (sliderLength * 10) + pan.value()) + (sliderLength * 8), y - h, sliderLength * 2, h)
+		pop()
+
+    text(`${i+1}
+56`, (i * (sliderLength * 10) + pan.value()) + (sliderLength * 8) + 4, y - h + 20)
+  }
+
+  // Rest modules 3m - less cuts
   stroke(0)
+  let xx = (sliderLength * 10 * module10Num) + pan.value()
+  switch (unitsAmount) {
+    case 9:
+      fill(106, 154, 178)
+      shortMod = rect(xx, y - h, 6 * sliderLength, h)
+
+      noStroke()
+      fill(0, 0, 0)
+      text(`1
+	168`, xx + 4, y - h + 20)
+
+			if(profileLength>=2800){
+				push()
+				strokeWeight(4)
+	      stroke(255, 255, 255)
+	      connector = line(xx, y - h * .666, xx, y - h / 3.5)
+				pop()
+			}
+
+      stroke(0)
+      fill(211, 249, 175)
+      shortMod = rect(xx + 6 * sliderLength, y - h, 3 * sliderLength, h)
+
+      noStroke()
+      fill(0, 0, 0)
+      text(`1
+	84`, xx + 6 * sliderLength + 4, y - h + 20)
+      break
+
+    case 8:
+      fill(71, 115, 137)
+      shortMod = rect(xx, y - h, 8 * sliderLength, h)
+
+      noStroke()
+      fill(0, 0, 0)
+      text(`1
+	224`, xx + 4, y - h + 20)
+
+	if(profileLength>=2800){
+			push()
+      strokeWeight(4)
+      stroke(255, 255, 255)
+      connector = line(xx, y - h * .666, xx, y - h / 3.5)
+      pop()
+		}
+      break
+
+    case 7:
+      fill(162, 227, 193)
+      shortMod = rect(xx, y - h, 4 * sliderLength, h)
+
+			push()
+      noStroke()
+      fill(0, 0, 0)
+      text(`1
+	112`, xx + 4, y - h + 20)
+			pop()
+
+			if(profileLength>=2800){
+			push()
+      strokeWeight(4)
+      stroke(255, 255, 255)
+      connector = line(xx, y - h * .666, xx, y - h / 3.5)
+      pop()
+}
+
+      fill(211, 249, 175)
+      shortMod = rect(xx + 4 * sliderLength, y - h, 3 * sliderLength, h)
+
+			push()
+      noStroke()
+      fill(0, 0, 0)
+      text(`1
+	84`, xx + 4 * sliderLength + 4, y - h + 20)
+			pop()
+      break
+
+    case 6:
+      fill(106, 154, 178)
+      shortMod = rect(xx, y - h, 6 * sliderLength, h)
+
+      noStroke()
+      fill(0, 0, 0)
+      text(`1
+	168`, xx + 4, y - h + 20)
+
+	if(profileLength>=2800){
+			push()
+      strokeWeight(4)
+      stroke(255, 255, 255)
+      connector = line(xx, y - h * .666, xx, y - h / 3.5)
+      pop()
+		}
+      break
+
+    case 5:
+      fill(123, 201, 212)
+      shortMod = rect(xx, y - h, 5 * sliderLength, h)
+
+      noStroke()
+      fill(0, 0, 0)
+      text(`1
+	140`, xx + 4, y - h + 20)
+
+	if(profileLength>=2800){
+			push()
+      strokeWeight(4)
+      stroke(255, 255, 255)
+      connector = line(xx, y - h * .666, xx, y - h / 3.5)
+      pop()
+		}
+      break
+
+    case 4:
+      fill(162, 227, 193)
+      shortMod = rect(xx, y - h, 4 * sliderLength, h)
+
+      noStroke()
+      fill(0, 0, 0)
+      text(`1
+	112`, xx + 4, y - h + 20)
+
+	if(profileLength>=2800){
+			push()
+      strokeWeight(4)
+      stroke(255, 255, 255)
+      connector = line(xx, y - h * .666, xx, y - h / 3.5)
+      pop()
+		}
+      break
+
+    case 3:
+      fill(211, 249, 175)
+      shortMod = rect(xx, y - h, 3 * sliderLength, h)
+
+      noStroke()
+      fill(0, 0, 0)
+      text(`1
+	84`, xx + 4, y - h + 20)
+
+	if(profileLength>=2800){
+			push()
+      strokeWeight(4)
+      stroke(255, 255, 255)
+      connector = line(xx, y - h * .666, xx, y - h / 3.5)
+      pop()
+		}
+      break
+
+    case 2:
+      fill(237, 255, 171)
+      shortMod = rect(xx, y - h, 2 * sliderLength, h)
+
+      noStroke()
+      fill(0, 0, 0)
+      text(`1
+56`, xx + 4, y - h + 20)
+
+if(profileLength>=2800){
+			push()
+      strokeWeight(4)
+      stroke(255, 255, 255)
+      connector = line(xx, y - h * .666, xx, y - h / 3.5)
+			pop()
+		}
+      break
+
+    case 1:
+      push()
+      stroke(0)
+      fill(211, 249, 175)
+      translate(-(sliderLength * 2), 0)
+      shortMod = rect(xx, y - h, 3 * sliderLength, h)
+
+      noStroke()
+      fill(0, 0, 0)
+      text(`1
+	84`, xx + 4, y - h + 20)
+
+	if(profileLength>=2800){
+      strokeWeight(4)
+      stroke(255, 255, 255)
+      connector = line(xx, y - h * .666, xx, y - h / 3.5)
+      pop()
+		}
+      break
+  }
 
   // units
   noFill()
-  unitsBar = rect(x + camera.value(), y, totalLength / slider.value(), h / 2)
+  stroke(0)
+  unitsBar = rect(x + pan.value(), y, totalLength / shrink.value(), h / 2)
 
   //single modules
   fill(100)
   for (let i = 0; i < modulesNum(); i++) {
-    module = rect(i * sliderLength + camera.value(), y, sliderLength, h / 2)
+    singleModule = rect(i * sliderLength + pan.value(), y, sliderLength, h / 2)
   }
 
+  // Long modules 3m - less price
   mod8Num = Math.floor(modulesNum() / 8)
   for (let i = 0; i < mod8Num; i++) {
+    push()
     fill(71, 115, 137)
     stroke(0)
-    mod8Module = rect(i * sliderLength * 8 + camera.value(), y + h / 2, sliderLength * 8, h)
-    fill(0, 0, 0)
-    noStroke()
+    let mod8M = rect(i * sliderLength * 8 + pan.value(), y + h / 2, sliderLength * 8, h)
+    pop()
+
+		noStroke()
+		fill(0)
+    textSize(1 * sliderLength)
     text(`${i+ 1}
-		224`, i * sliderLength * 8 + 4 + camera.value(), y + h - 10);
+		224`, i * sliderLength * 8 + 4 + pan.value(), y + h - 10)
   }
 
-  for (let i = 1; i <= connectors; i++) {
-    stroke(0, 0, 255)
-    connector = line(i * sliderLength * 16 + camera.value(), y + h, i * sliderLength * 16 + camera.value(), y + h * 2)
-    stroke(0)
-  }
-
+  // Rest modules 3m - less price
   let rest = modulesNum() - (mod8Num * 8)
   let c
 
   switch (rest) {
     case 1:
+      push()
       c = color('rgb(123,201,212)')
       translate(-(sliderLength * 8), 0)
       restModule = new Module(5, c, 140)
@@ -178,6 +432,7 @@ function draw() {
       translate(sliderLength * 5, 0)
       restModule1 = new Module(4, c, 112)
       restModule1.show()
+      pop()
       break;
 
     case 2:
@@ -214,14 +469,45 @@ function draw() {
       c = color('rgb(162,227,193)')
       restModule = new Module(4, c, 112)
       restModule.show()
+      push()
       c = color('rgb(211,249,175)')
       restModule1 = new Module(3, c, 84)
       translate(sliderLength * 4, 0)
       restModule1.show()
+      pop()
       break;
   }
-}
+  // connectors lines 3m - less cuts
+  if (profileLength % max3m == 0) {
+    for (let i = 1; i <= connectors; i++) {
+      push()
+      strokeWeight(4)
+      stroke(255, 255, 255)
+      connector = line(i * sliderLength * 10 + pan.value(), y - h * .666, i * sliderLength * 10 + pan.value(), y - h / 3.5)
+      pop()
+    }
+  } else {
+    for (let i = 1; i < connectors; i++) {
+      push()
+      strokeWeight(4)
+      stroke(255, 255, 255)
+      connector = line(i * sliderLength * 10 + pan.value(), y - h * .666, i * sliderLength * 10 + pan.value(), y - h / 3.5)
+      pop()
+    }
+  }
 
+	// connectors lines 3m - less price
+		for (let i = 1; i <= connectors2; i++) {
+
+			push()
+			strokeWeight(4)
+			stroke(255, 255, 255)
+			connector = line(i * sliderLength * 8 + pan.value(), y + h * .8, i * sliderLength * 8 + pan.value(), y + h / .8)
+			pop()
+		}
+
+
+}
 
 
 
@@ -230,7 +516,7 @@ class Module {
     this.n = n
     this.c = c
     this.type = type
-    this.newX = sliderLength * 8 * mod8Num + camera.value()
+    this.newX = sliderLength * 8 * mod8Num + pan.value()
     let modNum = Math.floor(modulesNum() / this.n)
   }
 
@@ -240,6 +526,12 @@ class Module {
     let lastMod = rect(this.newX, y + h / 2, sliderLength * this.n, h)
     fill(0, 0, 0)
     noStroke()
-    text('1 \n' + this.type, this.newX + 4, y + h - 10);
+    if (this.type == 56) {
+      text(`1
+56`, this.newX + 4, y + h - 10)
+    } else {
+      text(`1
+	${this.type}`, this.newX + 4, y + h - 10);
+    }
   }
 }
